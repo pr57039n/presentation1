cdfd571caea3651dca7146cb93bcc0498536bdc8
herimendoza <107810003+herimendoza@users.noreply.github.com>
2022-08-17 22:43:07 +0000

    Update presentation.sh

--- presentation.sh
+++ presentation.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 
+# case statement to select parameter
+
+echo -n "Choose your parameter from the following: genre, certificate, runtime, year, director, rating "
+read var_parameter;
+
+case $var_parameter in
+  
+  genre)
+  #code for genre
+  ;;
+  
+  certificate)
+  #code for certificate
+  ;;
+  
+  runtime)
+  #code for runtime
+  ;;
+  
+  year)
+  #code for year
+  ;;
+  
+  rating)
+  #code for rating
+  ;;
+  
+  *)
+  echo "unknown parameter.
+  ;;
+esac
